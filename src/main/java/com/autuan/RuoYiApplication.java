7364efdb99b049a27fdcfed105caef9956a3c007
Autuan.Yu <autuan.yu@gmail.com>
2020-12-17 13:55:11 +0800

    fastdfs 上传

--- src/main/java/com/autuan/RuoYiApplication.java
+++ src/main/java/com/autuan/RuoYiApplication.java
@@ -1,14 +1,17 @@
 package com.autuan;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 /**
  * 启动程序
  *
  * @author ruoyi
  */
+@Import(FdfsClientConfig.class)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuoYiApplication {
     public static void main(String[] args) {
