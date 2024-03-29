779374590e3c590d362e2d882686183d02d65b12
Autuan <yu@autuan.top>
2024-03-29 10:23:49 +0800

    Update to ruoyi-satoken

--- src/main/java/com/autuan/common/exception/custom/CustomRespondException.java
+++
@@ -1,8 +0,0 @@
-package com.autuan.common.exception.custom;
-
-public class CustomRespondException extends RuntimeException {
-
-    public CustomRespondException(String msg) {
-        super(msg);
-    }
-}
